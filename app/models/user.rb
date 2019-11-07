788addf0d90921b9f9a014ae341adb36f0f60fad
ViriCruz <viridiana.cruz.olivera@gmail.com>
2019-11-07 15:22:46 -0700

    remember token before creation of users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
   before_save   :downcase_email
+  before_create :create_remember_token
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, 
@@ -10,6 +12,13 @@ class User < ApplicationRecord
 
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
+
+  # Returns the hash digest of the given string.
+  def User.digest(string)
+  cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                BCrypt::Engine.cost
+  BCrypt::Password.create(string, cost: cost)
+  end
 
   # Returns a random token.
   def User.new_token
@@ -26,5 +35,10 @@ class User < ApplicationRecord
     # Converts email to all lower-case.
     def downcase_email
       self.email.downcase!
+    end
+
+    def create_remember_token
+      self.remember_token = User.new_token
+      self.remember_digest = User.digest(remember_token)
     end
 end
