ef14489767bd35c85ec66cdfc4d71e29a414034d
ViriCruz <viridiana.cruz.olivera@gmail.com>
2019-11-08 12:09:56 -0700

    create remember token and remember digest

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,9 @@ class ApplicationController < ActionController::Base
 
   def sign_in 
     @user = User.find_by(email: params[:session][:email].downcase)
+    # create new remember token here and save it in cookie permanent
+    @user.remember
+    # puts @user.remember_token contiene el token para guardar en la cookie
     if @user && @user.authenticate(params[:session][:password])
         flash.now[:success] = "Logged in"
         render "users/show"
--- app/models/user.rb
+++ app/models/user.rb
@@ -13,18 +13,14 @@ class User < ApplicationRecord
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
 
-  # Returns the hash digest of the given string.
-  def User.digest(string)
-  cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
-                                                BCrypt::Engine.cost
-  BCrypt::Password.create(string, cost: cost)
+  def remember
+    # actualiza atributo en base de datos y genera un nuevo token
+    update_attribute(:remember_digest, create_remember_token)
   end
-
-  # Returns a random token.
-  def User.new_token
-    SecureRandom.urlsafe_base64
+  
+  def forget
+    # actualiza atributo en base de datos a nulo 
   end
-
   # Returns true if the given token matches the digest.
   # def authenticated?(attribute, token)
   #   digest = send("#{attribute}_digest")
@@ -36,9 +32,21 @@ class User < ApplicationRecord
     def downcase_email
       self.email.downcase!
     end
+    # Returns the hash digest of the given string.
+    def User.digest(string)
+      cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                    BCrypt::Engine.cost
+      BCrypt::Password.create(string, cost: cost)
+    end
+  
+    # Returns a random token.
+    def User.new_token
+      SecureRandom.urlsafe_base64
+    end
 
     def create_remember_token
       self.remember_token = User.new_token
       self.remember_digest = User.digest(remember_token)
     end
+    
 end
