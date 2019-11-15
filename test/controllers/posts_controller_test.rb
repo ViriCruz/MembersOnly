ab8fa84ed76ca8e4e02a2ad81e8cf2a6f838449b
ViriCruz <viridiana.cruz.olivera@gmail.com>
2019-11-15 14:49:04 -0700

    some post tests added

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -1,18 +1,15 @@
 require 'test_helper'
 
 class PostsControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
-    get posts_new_url
-    assert_response :success
-  end
+  
 
   test "should get create" do
-    get posts_create_url
+    get posts_url
     assert_response :success
   end
 
   test "should get index" do
-    get posts_index_url
+    get posts_path
     assert_response :success
   end
 
@@ -26,15 +23,14 @@ class PostsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "user can't access create post [post posts/create] when not logged in" do
+    get new_post_url
+    follow_redirect!
+    assert_template 'sessions/new'
   end
 
   test "user can create post when logged in" do
   end
 
-  test "user can't create empty posts" do
-  end
-
-  test "user can't create titleless posts" do
-  end
+  
 
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,11 +3,20 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
   def setup
     @user = users(:ivan)
-    @post = Post.new(user_id: @user.id)
+    @post = Post.new(title: "d",body: "a",user_id: @user.id)
   end
 
   test "Post should be valid" do
     assert @post.valid?
   end
 
+  test "user can't create empty posts" do
+    @post.body = nil
+    assert_not @post.valid?
+  end
+
+  test "user can't create titleless posts" do
+    @post.title = nil
+    assert_not @post.valid?
+  end
 end
