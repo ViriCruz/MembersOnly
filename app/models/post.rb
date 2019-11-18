a83e7382708f04559b1cfbc7271201bb8eb0b351
ViriCruz <viridiana.cruz.olivera@gmail.com>
2019-11-18 10:45:38 -0700

    title length increased

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
     belongs_to :user
-    validates :title, presence: true, length: { in: 6..20 }
+    validates :title, presence: true, length: { in: 6..35 }
     validates :body, presence: true, length: { in: 15..300 }
     validates :user_id, presence: true
 end
