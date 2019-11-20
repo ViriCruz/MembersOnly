567eee6a8cb20c8047f24ba7fc3ce67bb8ff1f96
ViriCruz <viridiana.cruz.olivera@gmail.com>
2019-11-20 16:23:18 -0700

    detailed error messages in sign up form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,24 +1,25 @@
 class UsersController < ApplicationController 
   def show
-    @current_user = current_user
+    #@current_user = current_user
+    @current_user = User.find(params[:id])
 
     # validar que el current user sea igual al que hace las peticiones
     # denegar acceso a la vista de otros usuarios y solo autorizar acceso a la vista del usuario logueado
   end
 
   def new
+    @user = User.new
   end
 
   def create
     @user = User.new(user_params)
     if @user.save
       remember_user(@user)
-      flash.now[:success] = "User created successfully"
-      flash.now[:success] = "Login successfully"
+      flash[:success] = "Account created successfully"
+      flash[:info] = "Login successfully"
       @current_user = @user
       redirect_to @user
     else
-      flash.now[:danger] = "Check data input"
       render 'new'
     end
   end
