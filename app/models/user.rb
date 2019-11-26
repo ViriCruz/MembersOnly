88cc2a6e4aa604255dd3b3816780c81afd90c041
ViriCruz <viridiana.cruz.olivera@gmail.com>
2019-11-26 13:57:46 -0700

    changed encryption method by sha1.hexdigest

--- app/models/user.rb
+++ app/models/user.rb
@@ -39,8 +39,7 @@ class User < ApplicationRecord
 
   # Returns the hash digest of the given string.
   def self.digest(string)
-    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
-    BCrypt::Password.create(string, cost: cost)
+    Digest::SHA1.hexdigest(string)
   end
 
   private
